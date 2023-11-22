ca24602e20453653ec13be8de40eb8e40ba08d53
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2023-11-22 15:46:12 +0100

    fix SunderCount plugin

--- Details_SunderCount/Details_SunderCount.lua
+++ Details_SunderCount/Details_SunderCount.lua
@@ -5,29 +5,72 @@ local sunderCount = {
 	attribute = false,
 	spellid = false,
 	target = false,
-	author = "Matz",
+	author = "NoM0Re",
 	desc = "Shows who uses Sunder Armor.",
 	script_version = 2,
 	script = [[
-		local combat, container, instance = ...
+		local combat, instance_container, instance = ...
 		local total, top, amount = 0, 0, 0
+		
+		local spellsToCount = {
+			7386, -- Sunder Armor, shut be same for every expansion
+			20243, -- Devastate Rank 1
+			30016, -- Devastate Rank 2
+			30022, -- Devastate Rank 3
+			47497, -- Devastate Rank 4
+			47498, -- Devastate Rank 5
+			8647, -- Expose Armor, shut be same for every expansion
+		}
 
-		local sunderName = GetSpellInfo(11597)
-		local actors = combat:GetActorList(DETAILS_ATTRIBUTE_MISC)
-		for i, actor in ipairs(actors) do
+		for i, actor in ipairs(combat:GetActorList(4)) do
 			if actor:IsPlayer() and actor.spell_cast then
 				for spellName, count in pairs(actor.spell_cast) do
-					if spellName == sunderName then
-						container:AddValue(actor, count)
+					for _, spellID in ipairs(spellsToCount) do
+						if spellName == spellID then
+							instance_container:AddValue(actor, count)
+						end
 					end
 				end
 			end
 		end
-
-		total, top = container:GetTotalAndHighestValue()
-		amount = container:GetNumActors()
-
+		
+		total, top = instance_container:GetTotalAndHighestValue()
+		amount = instance_container:GetNumActors()
+		
 		return total, top, amount
+	]],
+	tooltip = [[
+		local Actor, Combat, Instance = ...
+		local Format = Details:GetCurrentToKFunction()
+
+		--get the cooltip object(we dont use the convencional GameTooltip here)
+		local GameCooltip = GameCooltip
+		
+		local spellsToCount = {
+			7386, -- Sunder Armor, same for every expansion
+			20243, -- Devastate Rank 1
+			30016, -- Devastate Rank 2
+			30022, -- Devastate Rank 3
+			47497, -- Devastate Rank 4
+			47498, -- Devastate Rank 5
+			8647, -- Expose Armor, same for every expansion
+		}
+		
+		local hoveredActorName = Actor.nome
+		for i, actor in ipairs(Combat:GetActorList(4)) do
+			if actor:IsPlayer() and actor.nome == hoveredActorName and actor.spell_cast then
+				for _, spellID in ipairs(spellsToCount) do
+					for spellName, count in pairs(actor.spell_cast) do
+						if spellName == spellID then
+							local localizedSpellName, _, Icon = GetSpellInfo(spellID)
+							GameCooltip:AddLine(localizedSpellName, count)
+							Details:AddTooltipBackgroundStatusbar()
+							GameCooltip:AddIcon(Icon, 1, 1, _detalhes.tooltip.line_height, _detalhes.tooltip.line_height)
+						end
+					end
+				end
+			end
+		end
 	]],
 	total_script = [[
 		local value, top, total, combat, instance = ...
@@ -37,8 +80,6 @@ local sunderCount = {
 		local value, top, total, combat, instance = ...
 		return string.format("%.1f", value / total * 100)
 	]],
-	tooltip = false,
-	notooltip = true,
 }
 
 Details:InstallCustomObject(sunderCount)
