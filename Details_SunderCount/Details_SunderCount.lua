84b80c485a3431e31cf4079c1ecf340a09d7e15f
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2023-11-29 15:10:22 +0100

    optimizations

--- Details_SunderCount/Details_SunderCount.lua
+++ Details_SunderCount/Details_SunderCount.lua
@@ -9,60 +9,59 @@ local sunderCount = {
 	desc = "Shows who uses Sunder Armor.",
 	script_version = 2,
 	script = [[
-		local combat, instance_container, instance = ...
+		local combat, CustomContainer, instance = ...
 		local total, top, amount = 0, 0, 0
 		
 		local spellsToCount = {
-			7386, -- Sunder Armor, shut be same for every expansion
-			20243, -- Devastate Rank 1
-			30016, -- Devastate Rank 2
-			30022, -- Devastate Rank 3
-			47497, -- Devastate Rank 4
+			7386, -- Sunder Armor
+			8647, -- Expose Armor
 			47498, -- Devastate Rank 5
-			8647, -- Expose Armor, shut be same for every expansion
+			47497, -- Devastate Rank 4
+			30022, -- Devastate Rank 3
+			30016, -- Devastate Rank 2
+			20243, -- Devastate Rank 1	
 		}
 
-		for i, actor in ipairs(combat:GetActorList(4)) do
+		for _, actor in ipairs(combat:GetActorList(4)) do
 			if actor:IsPlayer() and actor.spell_cast then
-				for spellName, count in pairs(actor.spell_cast) do
-					for _, spellID in ipairs(spellsToCount) do
-						if spellName == spellID then
-							instance_container:AddValue(actor, count)
+				for spellID, count in pairs(actor.spell_cast) do
+					for _, spell in ipairs(spellsToCount) do
+						if spellID == spell then
+							CustomContainer:AddValue(actor, count)
 						end
 					end
 				end
 			end
 		end
 		
-		total, top = instance_container:GetTotalAndHighestValue()
-		amount = instance_container:GetNumActors()
+		total, top = CustomContainer:GetTotalAndHighestValue()
+		amount = CustomContainer:GetNumActors()
 		
 		return total, top, amount
 	]],
 	tooltip = [[
 		local Actor, Combat, Instance = ...
-		local Format = Details:GetCurrentToKFunction()
 
-		--get the cooltip object(we dont use the convencional GameTooltip here)
 		local GameCooltip = GameCooltip
+		local GetSpellInfo = GetSpellInfo
 		
 		local spellsToCount = {
-			7386, -- Sunder Armor, same for every expansion
-			20243, -- Devastate Rank 1
-			30016, -- Devastate Rank 2
-			30022, -- Devastate Rank 3
-			47497, -- Devastate Rank 4
+			7386, -- Sunder Armor
+			8647, -- Expose Armor
 			47498, -- Devastate Rank 5
-			8647, -- Expose Armor, same for every expansion
+			47497, -- Devastate Rank 4
+			30022, -- Devastate Rank 3
+			30016, -- Devastate Rank 2
+			20243, -- Devastate Rank 1	
 		}
 		
-		local hoveredActorName = Actor.nome
-		for i, actor in ipairs(Combat:GetActorList(4)) do
-			if actor:IsPlayer() and actor.nome == hoveredActorName and actor.spell_cast then
-				for _, spellID in ipairs(spellsToCount) do
-					for spellName, count in pairs(actor.spell_cast) do
-						if spellName == spellID then
-							local localizedSpellName, _, Icon = GetSpellInfo(spellID)
+		local Owner = Actor.nome
+		for _, actor in ipairs(Combat:GetActorList(4)) do
+			if actor:IsPlayer() and actor.nome == Owner and actor.spell_cast then
+				for spellID, count in pairs(actor.spell_cast) do
+					for _, spell in ipairs(spellsToCount) do
+						if spellID == spell then
+							local localizedSpellName, _, Icon = GetSpellInfo(spell)
 							GameCooltip:AddLine(localizedSpellName, count)
 							Details:AddTooltipBackgroundStatusbar()
 							GameCooltip:AddIcon(Icon, 1, 1, _detalhes.tooltip.line_height, _detalhes.tooltip.line_height)
